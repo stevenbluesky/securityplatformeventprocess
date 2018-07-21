69e8054ea8ae589806b33a70070e202fa26f5d9c
fuwenjing <fuwenjing@isurpass.com.cn>
2018-07-21 14:19:57 +0800

    修复问题：设防撤防时，推送到告警平台的消息错误。

--- src/main/java/cn/com/isurpass/securityplatform/alarmone/AlarmoneMessageSender.java
+++ src/main/java/cn/com/isurpass/securityplatform/alarmone/AlarmoneMessageSender.java
@@ -64,9 +64,9 @@ public class AlarmoneMessageSender implements IAlarmMessageSender
 		alarmmessagemap.put("remoteonline", "R350");
 		alarmmessagemap.put("dscalarm", "E132");
 		alarmmessagemap.put("unalarmdscalarm", "R132");
-		alarmmessagemap.put("armstatus_0", "R401");
-		alarmmessagemap.put("armstatus_1", "E401");
-		alarmmessagemap.put("armstatus_3", "E441");
+		alarmmessagemap.put("armstatus_0", "E401");
+		alarmmessagemap.put("armstatus_1", "R401");
+		alarmmessagemap.put("armstatus_3", "R441");
 
 //		alarmmessagemap.put("passworderror5times", "");
 //		alarmmessagemap.put("bulliedopenlock", "");
