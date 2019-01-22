5994cffc2909479923f6e10d27e8f5ab4503a2ae
fuwenjing <fuwenjing@isurpass.com.cn>
2019-01-22 11:48:18 +0800

    支持第二家告警中心

--- src/main/java/cn/com/isurpass/securityplatform/alarmtwo/AlarmTwoClientInitializer.java
+++ src/main/java/cn/com/isurpass/securityplatform/alarmtwo/AlarmTwoClientInitializer.java
@@ -8,6 +8,8 @@ import io.netty.handler.timeout.IdleStateHandler;
 
 import java.util.concurrent.TimeUnit;
 
+import cn.com.isurpass.securityplatform.alarmone.EscapeCoder;
+
 
 public class AlarmTwoClientInitializer extends ChannelInitializer<SocketChannel> {
 
@@ -16,7 +18,7 @@ public class AlarmTwoClientInitializer extends ChannelInitializer<SocketChannel>
         ChannelPipeline pipeline = ch.pipeline();
 
         pipeline.addLast(new IdleStateHandler(180, 15, 15, TimeUnit.SECONDS));
-        //pipeline.addLast("escapecoder", new EscapeCoder());
+        pipeline.addLast("escapecoder", new EscapeCoder());
         pipeline.addLast("framer", new FixedLengthFrameDecoder(1));
         pipeline.addLast("handler", new AlarmTwoHeartBeatHandler());
 
